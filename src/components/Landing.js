ddabade130bdc209f87f3b97102604166ae55e42
Scott Coupar <scott.coupar@googlemail.com>
2021-04-27 22:32:14 +0100

    landing page styled

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import headshot from "../static/images/scott.jpeg";
 
 const Landing = () => {
     return (
         <>
+        
+        <div className = "landing-text">
+        <img className = "headshot"src = {headshot} alt = "headshot" height = "400px"/>
+        <h1 className = "name">S _ C O U P A R</h1>
+        <h2 className ="job-title">software developer</h2>
+        <button className = "button">hello.</button>
+        </div>
+        
+        
         </>
     )
 }
