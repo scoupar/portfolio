91eb9673cc08e8cb3d134a293bb36447c9150b84
Scott Coupar <scott.coupar@googlemail.com>
2021-04-29 23:06:35 +0100

    changed styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,9 +7,9 @@ const Contact = () => {
         <section id="contact">
             <h1 className ="title">Contact</h1>
             <div className = "social-icons">
-            <SocialIcon url ="https://github.com/scoupar" target ="_blank" network = "github" bgColor ="#ffbd59" className ="icon" style={{height:100, width:100}}/>
-            <SocialIcon url = "https://www.linkedin.com/in/scott-coupar/" target ="_blank" network = "linkedin" bgColor ="#ffbd59" className ="icon" style={{height:100, width:100}}/>
-            <SocialIcon url = "mailto:scott.coupar@googlemail.com" target ="_blank" network = "email" bgColor ="#ffbd59" className ="icon" style={{height:100, width:100}}/>
+            <SocialIcon url ="https://github.com/scoupar" target ="_blank" network = "github" bgColor ="whitesmoke" className ="icon" style={{height:100, width:100}}/>
+            <SocialIcon url = "https://www.linkedin.com/in/scott-coupar/" target ="_blank" network = "linkedin" bgColor ="whitesmoke" className ="icon" style={{height:100, width:100}}/>
+            <SocialIcon url = "mailto:scott.coupar@googlemail.com" target ="_blank" network = "email" bgColor ="whitesmoke" className ="icon" style={{height:100, width:100}}/>
             </div>
         </section>
         <footer>
