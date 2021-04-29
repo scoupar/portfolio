9d61b0ade95c3130171ec086b996e9a11723fa61
Scott Coupar <scott.coupar@googlemail.com>
2021-04-29 22:54:43 +0100

    astyled devicons

--- src/components/About.js
+++ src/components/About.js
@@ -15,17 +15,17 @@ const About = () => {
         <p>I have experience with Java, JavaScript and Python and I'm comfortable working on both front and back end applications. You can see some of my work in the projects section below.</p>
         {/* <button className = "button2">Projects</button> */}
         <div className ="devicon-container">
-        <DevIcon icon ="python" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="java" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="javascript" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="html5" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="css3" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="npm" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="visualstudio" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="postgresql" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="mongodb" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="git" height = {50} fill="#ffbd59"/>
-        <DevIcon icon ="github_badge" height = {50} fill="#ffbd59"/>
+        <DevIcon icon ="python" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="java" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="javascript" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="html5" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="css3" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="npm" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="visualstudio" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="postgresql" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="mongodb" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="git" height = {50} fill="whitesmoke"/>
+        <DevIcon icon ="github_badge" height = {50} fill="whitesmoke"/>
         </div>
         
 
