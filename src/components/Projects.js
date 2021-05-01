8aed8c5ea1590909f3f714142a88fae8141a4b57
Scott Coupar <scott.coupar@googlemail.com>
2021-05-01 17:43:38 +0100

    added youtube

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,13 +3,15 @@ import ModalVideo from 'react-modal-video';
 import DevIcon from 'devicon-react-svg';
 import ccnews from '../static/images/CCNews.png';
 import hellospace from '../static/images/Hello Space.png';
-import stockmanager from '../static/images/stockmanager.png'
+import stockmanager from '../static/images/stockmanager.png';
+import ReactPlayer from 'react-player';
 
 
 const Projects = () => {
 
     const [isOpenSpace, setIsOpenSpace] = useState(false);
     const [isOpenCC, setIsOpenCC] = useState(false);
+    const [isOpenStock, setIsOpenStock] = useState(false);
 
 
 
@@ -19,46 +21,69 @@ const Projects = () => {
         <h1 className="title">Projects</h1>
         <div className = "projects-container">
             <div className = "project-card">
-                <h1 className = "project-title">Hello Space!</h1>
-                <img className = "project-image"src = {hellospace} height = "150" width = "auto" alt= "screenshot"/>
-                <h2 className = "project-description">A full-stack education app</h2>
+                <div className = "project-title">
+                <h1>Hello Space! - an educational app</h1>
+                </div>
+                <div className ="project-image">
+                <ReactPlayer url="https://youtu.be/tW9jmAHcNuE" controls />
+                </div>
+                <div className = "project-text">
+                <p>Built using React for the front end and an Express server to perform CRUD operations on a MongoDb database. The app also fetches data from multiple web API's (NASA and SpaceDevs) and displays selected data to the user.</p>
+                <p>Libraries such as HighCharts, Leaflet and Canvas are used to display information and create an interactive learning environment for the user.</p>
+                <span><p>Built with <a href="https://github.com/thestylishcoder" target ="blank" className = "team-link">Anna Cunningham</a> and <a href ="https://github.com/reneengraham" target ="blank" className = "team-link">Renee Graham</a></p></span>
+                <p></p>
+                <a className = "demo-button" href= "https://github.com/scoupar/SpaceAppProject" target ="_blank">View Code</a>
+                </div>
                 <div className = "project-devicon-container">
                 <DevIcon className = "project-devicon" icon ="javascript" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="html5" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="css3" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="mongodb" height = {40} fill="whitesmoke"/>
                 </div>
-                <ModalVideo channel ="youtube" autoplay isOpen={isOpenSpace} videoId="tW9jmAHcNuE" onClose={() => setIsOpenSpace(false)}   />
-                <button className = "demo-button" onClick ={() => setIsOpenSpace(true)}>View Demo</button>
-                <a className = "demo-button" href= "https://github.com/scoupar/SpaceAppProject" target ="_blank">View Code</a>
+
+            
             </div>
             <div className = "project-card">
-                <h1 className = "project-title">CC News</h1>
-                <img className = "project-image"src ={ccnews} height ="150" width= "auto" alt ="screenshot"/>
-                <h2 className = "project-description">A full-stack news app</h2>
+                <div className = "project-title">
+                <h1>CC News - a full-stack news app</h1>
+                </div>
+                <div className = "project-image">
+                <ReactPlayer url="https://youtu.be/3CIGxUUhcpw" controls/>
+                </div>
+                <div className = "project-text">
+                    <p>Created with a react front-end which is connected to a spring boot Java back-end. The app features a reader-focused front section where news articles can be viewed and filtered by category and journalist. A keyword search function is also used to filter articles by their headline. </p>
+                    <p>The site also features an admin section which allows the user to add, edit and delete the articles and journalist information that is displayed in the front end.</p>
+                    <p>Additional features include a fully working weather widget and 'breaking news' section that pulls real news stories from the News API and updates on a regular basis. </p>
+                    <span><p>Built with <a href="https://github.com/tinahouston" target ="blank" className = "team-link">Tina Houston</a>, <a href ="https://github.com/mz-biddy01" target ="blank" className = "team-link">Helen Aguda</a> and <a href ="https://github.com/gregor69" target ="blank" className = "team-link">Gregor Simpson</a></p></span>
+                    <a className = "demo-button" href= "https://github.com/scoupar/Java_News_App_Frontend" target ="_blank">View Code</a>
+                </div>
+                
                 <div className = "project-devicon-container">
                 <DevIcon className = "project-devicon" icon ="java" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="javascript" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="html5" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="css3" height = {40} fill="whitesmoke"/>
                 </div>
-                <ModalVideo channel ="youtube" autoplay isOpen={isOpenCC} videoId="3CIGxUUhcpw" onClose={() => setIsOpenCC(false)} />
-                <button className = "demo-button" onClick ={() => setIsOpenCC(true)}>View Demo</button>
-                <a className = "demo-button" href= "https://github.com/scoupar/Java_News_App_Frontend" target ="_blank">View Code</a>
+
             </div>
             <div className = "project-card">
-                <h1 className = "project-title">Stock Manager</h1>
-                <img className = "project-image"src ={stockmanager} height ="150" width= "auto" alt ="screenshot"/>
-                <h2 className = "project-description">A full-stack stock-<br></br>management app</h2>
+                <div className = "project-title">
+                <h1>Stock Manager - a full-stack stock management app</h1>
+                </div>
+                <div className = "project-image">
+                <ReactPlayer url ="https://youtu.be/z43hjhUEKd0" controls/>
+                </div>
+                <div className = "project-text">
+                    <p>fgfd afdsf dfsd</p>
+                <a className = "demo-button" href= "https://github.com/scoupar/Stock_Manager_Project" target ="_blank">View Code</a>
+                </div>
                 <div className = "project-devicon-container">
                 <DevIcon className = "project-devicon" icon ="python" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="postgresql" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="html5" height = {40} fill="whitesmoke"/>
                 <DevIcon className = "project-devicon" icon ="css3" height = {40} fill="whitesmoke"/>
                 </div>
-                <ModalVideo channel ="youtube" autoplay isOpen={isOpenCC} videoId="3CIGxUUhcpw" onClose={() => setIsOpenCC(false)} />
-                <button className = "demo-button" onClick ={() => setIsOpenCC(true)}>View Demo</button>
-                <a className = "demo-button" href= "https://github.com/scoupar/Stock_Manager_Project" target ="_blank">View Code</a>
+
             </div>
         </div>
         </section>
