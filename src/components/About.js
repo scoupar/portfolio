e2520a099772eefc816504fe3b0e741e2091c06c
Scott Coupar <scott.coupar@googlemail.com>
2021-05-06 11:22:41 +0100

    changed about

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,10 @@ const About = () => {
         <br></br>
         <h1 className = "title">Hello,</h1>
         <div className = "about-body">
-        <p>I'm Scott Coupar - a full-stack developer based in Glasgow, Scotland. After making the decision to change careers, I have recently graduated from an intensive Professional Software Development course at CodeClan. I'm now looking for opportunites to continue developing both software and myself.</p>
+        <p>I'm Scott Coupar - a full-stack developer based in Glasgow, Scotland. After making the decision to change careers, I have recently graduated from an intensive Professional Software Development course at CodeClan. </p>
+        <p>The course has not only taught me the most relevant and valuable skills required to be a software developer, it has also awoken a desire in myself to continuously improve and learn. I have experience with Python, Java and JavaScript and having learned the foundations of these 3 languages in a 16-week period I am now confident in my abilities to pick up new languages and skills whenever required.   
+        </p>
+        <p>I'm now looking for opportunites to continue developing both software and myself.</p>
         <p>You can see some of my work in the projects section below.</p>
         <br></br>
         <NavHashLink smooth to = "/#projects"
