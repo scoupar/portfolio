38e7c07df3663fd951aedeab03e2bac73007256e
Scott Coupar <scott.coupar@googlemail.com>
2021-07-27 16:56:33 +0100

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,43 +1,56 @@
-import React from 'react';
-import {NavHashLink} from 'react-router-hash-link';
-import logo from '../static/images/logo.png';
+import React from "react";
+import { NavHashLink } from "react-router-hash-link";
+import logo from "../static/images/logo.png";
 
 const NavBar = () => {
-    return(
-        
-        <div className = "navbar-container">
-        <div id = "logo">
-        <a href = "./">
-            <img className = "logo" src ={logo} alt ="logo" height = "75px"/>
+  return (
+    <div className="navbar-container">
+      <div id="logo">
+        <a href="./">
+          <img className="logo" src={logo} alt="logo" height="75px" />
         </a>
-        </div>
-            <div className = "navbar-button">
-                <NavHashLink smooth to = "/#about"
-                scroll ={(el) => el.scrollIntoView({behavior: 'smooth', block: 'center'})}
-                className = "item"
-                activeClassName = "activeRoute">
-                About
-                </NavHashLink>
-            </div>
-            <div className = "navbar-button">
-                <NavHashLink smooth to = "/#projects"
-                scroll ={(el) => el.scrollIntoView({behavior: 'smooth', block: 'start'})}
-                className = "item"
-                activeClassName = "activeRoute">
-                Projects
-                </NavHashLink>
-            </div>
-            <div className = "navbar-button">
-                <NavHashLink smooth to = "/#contact"
-                scroll ={(el) => el.scrollIntoView({behavior: 'smooth', block: 'center'})}
-                className = "item"
-                activeClassName = "activeRoute">
-                Contact
-                </NavHashLink>
-            </div>
-        </div>
-        
-    )
-}
+      </div>
+      <div className="navbar-button">
+        <NavHashLink
+          smooth
+          to="/#about"
+          scroll={(el) =>
+            el.scrollIntoView({ behavior: "smooth", block: "center" })
+          }
+          className="item"
+          activeClassName="activeRoute"
+        >
+          About
+        </NavHashLink>
+      </div>
+      <div className="navbar-button">
+        <NavHashLink
+          smooth
+          to="/#projectslist"
+          scroll={(el) =>
+            el.scrollIntoView({ behavior: "smooth", block: "start" })
+          }
+          className="item"
+          activeClassName="activeRoute"
+        >
+          Projects
+        </NavHashLink>
+      </div>
+      <div className="navbar-button">
+        <NavHashLink
+          smooth
+          to="/#contact"
+          scroll={(el) =>
+            el.scrollIntoView({ behavior: "smooth", block: "center" })
+          }
+          className="item"
+          activeClassName="activeRoute"
+        >
+          Contact
+        </NavHashLink>
+      </div>
+    </div>
+  );
+};
 
 export default NavBar;
